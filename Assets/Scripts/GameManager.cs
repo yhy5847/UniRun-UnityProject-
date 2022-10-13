cd583ed53994a6c8f8d64940c71e0a53676d43f5
yhy5847 <yhy5847@gmail.com>
2022-10-13 17:01:39 +0900

    ver.1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+    
+    public static GameManager instance;
+
+    public bool isGameover = false;
+    public Text scoreText;
+    public GameObject gameoverUI;
+
+    private int score = 0;
+
+    void Awake() {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("씬에 두개 이상의 게임 매니저가 존재합니다!");
+            Destroy(gameObject);
+        }
+    }
+
+    void Update() {
+        if(isGameover && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void AddScore(int newScore) {
+        if (!isGameover)
+        {
+            score += newScore;
+            scoreText.text = "Score : " + score;
+        }
+    }
+
+    public void OnPlayerDead() {
+        isGameover = true;
+        gameoverUI.SetActive(true);
+    }
+    
+}
